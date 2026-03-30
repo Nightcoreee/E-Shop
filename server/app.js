a40adf5a0467cecb9978a782f0aebbfba65c32f6
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-03-31 00:01:56 +0700

    refactor: setup server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
-import {config} from "dotenv"
-const express = require('express');
+import {config} from "dotenv";
+import cors from "cors";
+import express from "express";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 
 config({ path: "./config/.env"});
 
-app.use()
+app.use(cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(fileUpload({
+    tempFileDir:  "./uploads",
+    useTempFiles: true,
+}));
+
+export default app;
