578a5955bd2437346343b452758557d07520e97c
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-04-26 23:58:28 +0700

    chore: add reset pass

---
+++ server/utils/genResetPassword.Token.js
@@ -0,0 +1,14 @@
+import crypto from "crypto";
+
+export const genResetPasswordToken = () => {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  const resetPasswordExpireTime = Date.now() + 15 * 60 * 1000;
+
+  return { resetToken, hashedToken, resetPasswordExpireTime };
+};
