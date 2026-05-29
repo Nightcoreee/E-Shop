a04b7897981188219eb26fbaa26841131a7dcc52
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-05-30 04:24:02 +0700

    feat: add fetchAllOrders route for admin to retrieve all orders

--- server/router/order.Routes.js
+++ server/router/order.Routes.js
@@ -3,7 +3,7 @@ import {
     placeNewOrder,
     fetchSingleOrder,
     fetchMyOrders,
-
+    fetchAllOrders
 } from "../controllers/order.Controller.js";
 import { 
     isAuthenticated,
@@ -15,5 +15,7 @@ const router = express.Router();
 router.post("/new", isAuthenticated, placeNewOrder);
 router.get("/single/:orderId", isAuthenticated, fetchSingleOrder);
 router.get("/myorders", isAuthenticated, fetchMyOrders);
+
+router.get("/admin/all", isAuthenticated, authorizeRoles("Admin"), fetchAllOrders);
 
 export default router;
